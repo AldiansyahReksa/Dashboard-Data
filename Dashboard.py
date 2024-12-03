4b0683f368f9584e2e8b7e1cea20bab2af168030
AldiansyahReksa <118238511+AldiansyahReksa@users.noreply.github.com>
2024-12-03 21:41:55 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -144,10 +144,12 @@ elif tabs == "Analisis Keseluruhan":
     ax.tick_params(axis='x', rotation=90)  # Mengubah label sumbu X menjadi vertikal
     st.pyplot(fig)
 
-    # Line chart untuk tren total kunjungan tahunan per pintu masuk
-    fig_trend, ax_trend = plt.subplots(figsize=(10, 6))
-    sns.lineplot(data=total_data, x="Pintu Masuk", y="Total Kunjungan", marker='o', ax=ax_trend)
-    ax_trend.set_title("Tren Kunjungan Wisata per Pintu Masuk")
-    ax_trend.set_xlabel("Pintu Masuk")
-    ax_trend.set_ylabel("Total Kunjungan")
-    st.pyplot(fig_trend)
+# Menambahkan Line Chart (Trend Kunjungan Bulanan) secara vertikal
+fig_trend, ax_trend = plt.subplots(figsize=(10, 6))
+sns.lineplot(data=bulan_data, y="Bulan", x="Total Kunjungan", marker='o', ax=ax_trend)
+ax_trend.set_title(f"Tren Kunjungan Bulanan di {pintu_pilihan}")
+ax_trend.set_ylabel("Bulan")
+ax_trend.set_xlabel("Total Kunjungan")
+ax_trend.tick_params(axis='y', rotation=0)  # Mengubah label sumbu Y menjadi horizontal
+st.pyplot(fig_trend)
+
